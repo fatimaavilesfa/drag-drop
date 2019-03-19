84ba9e874500a0f6e58fafee7cbe1fe990bd256e
fatimaavilesfa <fatimaaviles1612@gmail.com>
2019-03-19 16:27:39 -0600

    able to copy from 1st list to child list move items in child list and no extra copy inside

--- angular/src/app/components/list/list.component.ts
+++ angular/src/app/components/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem,  CdkDragStart, CdkDragEnd, CdkDragMove } from '@angular/cdk/drag-drop';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { MatList } from '@angular/material';
+import {CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem,  CdkDragStart, CdkDragEnd, CdkDragMove, copyArrayItem } from '@angular/cdk/drag-drop';
 
 
 
@@ -10,96 +11,94 @@ import {CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem,  CdkDragStart,
 })
 export class ListComponent implements OnInit {
 
+  @ViewChild(MatList, { read: ElementRef }) child: ElementRef;
 
   data: any = {
     name: "",
     title: ""
   };
+
   empty:any;
+
   icons: any = [1, 2, 3];
+
   items: any = [4];
+
   childItems: any = [5];
-  // items: any = [{
-  //   state : "",
-  //   position : ""
-  // }];
 
+  _currentField;
 
-  state = "";
-  position = "";
-  X : number;
-  Y : number;
-
-
-
-
-  constructor() { }
+  _currentIndex;
 
   ngOnInit() {
-    this.resetList();
+
   }
 
-  private resetList() {
-    this.empty = [];
-    setTimeout(() => {
-      this.empty = this.icons.slice();
-    }, 0);
+  dragStart(event: CdkDragStart) {
+    this._currentIndex = this.icons.indexOf(event.source.data); // Get index of dragged type
+    this._currentField = this.child.nativeElement.children[this._currentIndex]; // Store HTML field
   }
 
-
-  drop(event: CdkDragDrop<any>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+  moved(event: CdkDragMove) {
+    // Check if stored HTML field is as same as current field
+    if (this.child.nativeElement.children[this._currentIndex] !== this._currentField) {
+      // Replace current field, basically replaces placeholder with old HTML content
+      this.child.nativeElement.replaceChild(this._currentField, this.child.nativeElement.children[this._currentIndex]);
     }
   }
+
+  itemDropped(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(this.icons, event.previousIndex, event.currentIndex);
+    } else {
+      this.addField(event.item.data, event.currentIndex);
+    }
+  }
+
+  addField(fieldType: any, index: number) {
+    this.childItems.splice(index, 0, fieldType);
+  }
+
+  // constructor() { }
+
+
+  // on init  this.resetList();
+  // private resetList() {
+  //   this.empty = [];
+  //   setTimeout(() => {
+  //     this.empty = this.icons.slice();
+  //   }, 0);
+  // }
+
+
+  // drop(event: CdkDragDrop<any>) {
+  //   if (event.previousContainer === event.container) {
+  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  //   } else {
+  //     transferArrayItem(event.previousContainer.data,
+  //                       event.container.data,
+  //                       event.previousIndex,
+  //                       event.currentIndex);
+  //   }
+  // }
+
+
+
+  // addToList(event: CdkDragDrop<string[]>) {
+  //   this.childItems.push(this.empty[event.previousIndex]);
+  //   this.resetList();
+  // }
+
+
 
   edit() {
     console.log(this.items);
     console.log(this.data );
-    console.log(this.items.position);
-    console.log(this.items.X);
-    console.log(this.items.Y);
   }
 
   reciveData($event) {
     this.data = $event;
   }
-
-  dragStarted(event: CdkDragStart) {
-    this.items.state = 'dragStarted';
-
-  }
-
-  dragEnded(event: CdkDragEnd) {
-    this.items.state = 'dragEnded';
-    console.log(event);
-  }
-
-  dragMoved(event: CdkDragMove) {
-    this.items.position = ` Position X: ${event.pointerPosition.x} - Y: ${event.pointerPosition.y}`;
-    this.items.X = event.pointerPosition.x;
-    this.items.Y = event.pointerPosition.y;
-  }
-
-  concat() {
-    if( this.items.X < this.items.Y ) {
-      console.log("yes");
-    } else {
-      console.log("no");
-    }
-  }
-
-
-  addToList(event: CdkDragDrop<string[]>) {
-    this.childItems.push(this.empty[event.previousIndex]);
-    this.resetList();
-  }
-
 
 
 }
